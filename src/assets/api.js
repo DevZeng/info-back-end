d5be255b6daa264b768ab4c1171d5cb71e1a3239
ZENG <zeng@ZENGdeiMac.local>
2019-04-21 23:00:20 +0800

    dix

--- src/assets/api.js
+++ src/assets/api.js
@@ -9,8 +9,8 @@ const TXWebService = 'http://apis.map.qq.com/ws/district/v1/'
 const TXKey = 'FF2BZ-H34WP-GQPDC-VFKIS-P7DDH-BCFNG'
 // const host = 'http://192.168.3.22:8090/'
 // const host = 'http://admin.sennkisystem.cn/'
-// const host = 'http://app.ankekan.com/'
-const host = 'http://203.195.129.30:83/'
+ const host = 'http://app.ankekan.com/'
+//const host = 'http://203.195.129.30:83/'
 
 /*
   配置 axios
